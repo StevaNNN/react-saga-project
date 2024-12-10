a5651f3b0ad4b01b05697dd28cbea814985ba4de
sstojanovic <stevan.stojanovic@level.com>
2024-12-10 07:41:57 +0100

    u

---
+++ src/LiveCodingExample.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./LoginForm.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formState, setFormState] = useState();
+
+  const hanldeEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const hanldePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const sendUser = async () => {
+      const request = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        }),
+      });
+      const data = await request.json();
+      setFormState(data);
+    };
+
+    sendUser();
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", gap: 20, padding: 20 }}
+    >
+      <h1>Brand</h1>
+      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+        <h2>Login</h2>
+        <p>Access your RemotePass account</p>
+        <form
+          style={{ display: "flex", flexDirection: "column", gap: 40 }}
+          onSubmit={handleFormSubmit}
+        >
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            <input id="email" value={email} onChange={hanldeEmailChange} />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              value={password}
+              onChange={hanldePasswordChange}
+            />
+          </div>
+          <button type="submit" disabled={!email || !password}>
+            Sign in
+          </button>
+        </form>
+      </div>
+      {formState?.message && (
+        <div>
+          <p>Please check your credentials and try again.</p>
+          <button>Forgot Password</button>
+        </div>
+      )}
+      {formState?.accessToken && "Hello user"}
+    </div>
+  );
+};
+
+export default LoginPage;
